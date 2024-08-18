d79cdf6c917b212103db37bac70c565ad6ed5c00
mucahitkarakus <mocococo.80@gmail.com>
2024-08-18 17:03:46 +0300

    router login and register done

---
+++ backend/routers/auth.router.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const User = require("../models/user/user")
+const router = express.Router()
+const { v4: uuidv4 } = require("uuid")
+const jwt = require("jsonwebtoken")
+
+const secretKey = "My Secret Key My Secret Key 123."
+const options = {
+    expiresIn: "1d"
+}
+
+router.post("/register", async (req, res) => {
+    try {
+        const user = new User(req.body);
+        user._id = uuidv4();
+        user.createdDate = new Date();
+        user.isAdmin = false;
+
+        const checkEmail = await User.findOne({ email: user.email })
+        if (checkEmail != null) {
+            res.status(403).json({ message: "Bu mail adresi daha önce kullanılmıştır" })
+        } else {
+            await user.save()
+            const token = jwt.sign({}, secretKey, options)
+            let model = { token: token, user: user }
+            res.json(model)
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        let user = await User.findOne({ email: email })
+        if (user == null) {
+            res.status(403).json({ message: "Kullanıcı bulunamadı" })
+        } else {
+            if (user.password != password) {
+                res.status(403).json({ message: "Şifre yanlış!" })
+            } else {
+                const token = jwt.sign({}, secretKey, options)
+                let model = { token: token, user: user }
+                res.json(model)
+            }
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+});
+module.exports = router
