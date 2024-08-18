76798cb33589be2dffc915f58690edc7abd06165
mucahitkarakus <mocococo.80@gmail.com>
2024-08-18 17:49:11 +0300

    login page done

--- frontend/src/app/components/layouts/layouts.component.ts
+++ frontend/src/app/components/layouts/layouts.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-laouts',
   standalone: true,
   imports: [SharedModule],
-  templateUrl: './laouts.component.html',
-  styleUrl: './laouts.component.css'
+  templateUrl: './layouts.component.html',
+  styleUrl: './layouts.component.css',
 })
 export class LaoutsComponent {
-
+ 
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,18 @@
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication, BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { importProvidersFrom } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { provideHttpClient } from "@angular/common/http";
+import { routes } from "./app/app.routes";
 
-bootstrapApplication(AppComponent, {
-  providers: [
+bootstrapApplication(AppComponent,{
+  providers:[
     provideHttpClient(),
-    importProvidersFrom(BrowserModule, CommonModule, RouterModule.forRoot([])),
-  ],
-});
+    importProvidersFrom(
+      BrowserModule,
+      CommonModule,
+      RouterModule.forRoot(routes)
+    )
+  ]
+})
