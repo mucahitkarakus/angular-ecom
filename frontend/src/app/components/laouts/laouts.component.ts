7d72d3a7fba77e6a850eaca635146ae9836bcdf5
mucahitkarakus <mocococo.80@gmail.com>
2024-08-18 17:09:04 +0300

    ng create login register home and layouts

---
+++ frontend/src/app/components/laouts/laouts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-laouts',
+  standalone: true,
+  imports: [],
+  templateUrl: './laouts.component.html',
+  styleUrl: './laouts.component.css'
+})
+export class LaoutsComponent {
+
+}
