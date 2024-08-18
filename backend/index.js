f0f6f2f1ffff6cbb751164c0e81c0738e4f34f71
mucahitkarakus <mocococo.80@gmail.com>
2024-08-18 17:03:56 +0300

    connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const connection = require("./database/db")
+const authRouter = require("./routers/auth.router")
 
 app.use(express.json())
 app.use(cors())
 
+app.use("/api/auth", authRouter)
 
+
+connection()
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Uygulama http://localhost:5000 portunda ayağa kalktı"))
